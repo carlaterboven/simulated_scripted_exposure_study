c2069d86ddabcbf5588e7bfd98e3f34c9e2254a1
carlaterboven <carla.terboven@student.hpi.de>
2023-01-09 21:54:00 +0100

    Add example recording

--- src/gps_mock.py
+++ src/gps_mock.py
@@ -8,8 +8,8 @@ class GpsMock:
         self.__alt = 0
         self.__time = 0
         self.__logid = 0
-        #mock_path = '../study_monitoring_data/log_test_balcony.csv'
-        mock_path = '../simulation_data/simulation_data.csv'
+        mock_path = '../study_monitoring_data/log_test_area.csv'
+        #mock_path = '../simulation_data/simulation_data.csv'
         self.__mock_file = pd.read_csv(mock_path)
 
     def __del__(self):
@@ -21,12 +21,12 @@ class GpsMock:
         # return [uniform(52.393, 52.394), uniform(13.0409, 13.0413)] # random gps generation for testing
 
     def read_time_series(self):
-        #self.__time = self.__mock_file.loc[self.__logid, 'time']
-        #self.__lat = self.__mock_file.loc[self.__logid, 'lat']
-        #self.__lon = self.__mock_file.loc[self.__logid, 'lon']
-        #self.__alt = self.__mock_file.loc[self.__logid, 'alt']
-        self.__lat = self.__mock_file.loc[self.__logid, 'latitude_index']
-        self.__lon = self.__mock_file.loc[self.__logid, 'longitude_index']
+        self.__time = self.__mock_file.loc[self.__logid, 'time']
+        self.__lat = self.__mock_file.loc[self.__logid, 'lat']
+        self.__lon = self.__mock_file.loc[self.__logid, 'lon']
+        self.__alt = self.__mock_file.loc[self.__logid, 'alt']
+        #self.__lat = self.__mock_file.loc[self.__logid, 'latitude_index']
+        #self.__lon = self.__mock_file.loc[self.__logid, 'longitude_index']
         self.__logid += 1
 
     def get_lat(self):
